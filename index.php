450fb3437937827c06f3335ba383c598697255e3
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2023-05-30 21:29:58 -0300

    vetores
    atividade de 07 exercicios em PHP com o auxílio do ChatGPT.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicios em Arrays</title>
+</head>
+<body>
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
+    
+</body>
+</html>
